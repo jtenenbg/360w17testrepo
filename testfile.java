e62d2a4aaed75e3f0df5501648c823b4e8ac83af
ntrinh139 <1309.nguyentrinh@gmail.com>
2017-01-25 22:55:13 -0800

    group1rocks

--- testfile.java
+++ testfile.java
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* Group 4 rocks! */
-/* Group 1 rocks! */
-/* Group 1 rocks! */
-=======
-/* TCSS 360 rocks! */
-/* Group 1 rocks!  */
->>>>>>> 82874077105a8bfc7a339c4f5ee249aa099c1dfc
-=======
-Group 3 rocks!
->>>>>>> b5fd386c10f15cc75c5e4575f254a01b02fc7433
+Group 1 Rocks!
